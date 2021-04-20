c9ba55214ce9a276ebb379a05d895adad3706756
lpaqkosw <leejaehoon@ais-info.co.jp>
2021-04-20 16:21:00 +0900

    edu

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import "./css/common.css";
-import React from "react";
+import React, { useEffect } from "react";
 
 import Top from "./components/top";
 import Mid from "./components/mid";
@@ -8,6 +8,19 @@ import Bot from "./components/bot";
 import Button from "./components/Button";
 import Reduxexample from "./components/example";
 function App() {
+  useEffect(async () => {
+    const parameters = { "test parameter": "TESTEST" };
+    const raw = await fetch("http://localhost:8030/api/main", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify(parameters),
+    });
+    const data = await raw.json();
+    console.log(data);
+  }, []);
   return (
     <div className="App">
       <Top></Top>
